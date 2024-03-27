a5b33c980d18fa2ec94aa4d8a4c0c9ab5d6b2dfe
henriquerojo <henriquerojo10@gmail.com>
2024-03-26 22:11:05 -0300

    conceitos básicos de javascript

---
+++ aula02-operadores/operadoresMat.js
@@ -0,0 +1,40 @@
+let numero1 = 6;
+let numero2 = 2;
+
+console.log("  ");
+console.log("------------------------------");
+console.log(`Número 1: ${numero1}`);
+console.log(`Número 2: ${numero2}`); 
+console.log("------------------------------");
+
+
+
+// operadores aritméticos
+
+//soma
+let soma = numero1 + numero2; 
+console.log("A soma é " + soma); // concatenação
+
+//const 
+const CPF = "123.345.678-00";
+console.log(CPF);
+
+// subtração
+let subtracao = numero1 - numero2;
+console.log("A subtração é: " + subtracao);
+
+//divisao
+let divisao = numero1 / numero2;
+console.log("A divisão é: " + divisao);
+
+//multiplicação
+let multiplicacao = numero1 * numero2;
+console.log("A multiplicação é " + multiplicacao);
+
+//modulo
+let resto = numero1 % numero2;
+console.log("O resto é: " + resto);
+
+//potencia
+let potencia = numero1 ** numero2;
+console.log("A potência é: " + potencia);
