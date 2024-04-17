147d9fa37277b64cfb0afb8484639fef84ebd2f5
henriquerojo <henriquerojo10@gmail.com>
2024-04-16 22:14:03 -0300

    intereação do site

--- aula06-course-inovation/script.js
+++ aula06-course-inovation/script.js
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', function()
         },
         {
             id: 3,
-            name: "Boof of javascript",
+            name: "Book of javascript",
             category: "books",
             price: 100
         },
@@ -32,10 +32,15 @@ document.addEventListener('DOMContentLoaded', function()
     let cart = [];
 
     const filterCategory = document.getElementById('filterCategory');
-    const list = document.getElementById('listProducts');
+    const renderListProducts = document.getElementById('listProducts');
     const totalCart = document.querySelector('#totalCart');
 
-    function listProducts(category)
+    /**
+     * Função que renderiza a lista de produtos
+     * @param category - nome da categoria selecionada na função
+     */
+
+    function listProducts(categorySelected)
     {
     // const productsMutate = products.forEach(product => {}); // void
     // console.log("======== FOR EACH: ========")
@@ -48,21 +53,49 @@ document.addEventListener('DOMContentLoaded', function()
     // console.log(productsMutate2);
     const productsToCategory = products.filter((product) => 
     {
-    if (category === 'all')
+    if (categorySelected === 'all')
     {
         return product;
     }
-    else if (product.category === category)
+    else if (product.category === categorySelected)
     {
         return product;
     }
     }); // ELE FILTRA O QUE ATENDER A VALIDAÇÃO
-    productsToCategory.forEach((productBLA) => 
+    productsToCategory.forEach((product) => 
     {
-        console.log("product: ", productBLA);
+        // console.log("product: ", product);
+        const itemLI  = document.createElement("li");
+        itemLI.textContent = `${product.name} - US$ ${product.price}`;
+        itemLI.classList.add('teste');
+        // itemLI.setAttribute('id', product.name);
+
+        const buttonAddCard = document.createElement("button");
+        buttonAddCard.textContent = "Add to Cart";
+        buttonAddCard.onclick = function()
+        {
+            addProductToCart(product);
+            console.log("cart: ", cart);
+            calculateTotalCart();
+        }
+        itemLI.appendChild(buttonAddCard);
+        renderListProducts.appendChild(itemLI);
     });
             
 
+    }
+    // função que adiciona um produto ao carrinho
+    function addProductToCart(product)
+    {
+        // cart.push()
+        cart.push(product);
+    }
+    // função que calcula o total do carrinho
+    function calculateTotalCart()
+    {
+        // soma o preço de todos os produtos do carrinho
+        const total = cart.reduce((accumalator, current) => accumalator + current.price, 0);
+        totalCart.textContent = `${total}`;
     }
     listProducts('all');
 }
